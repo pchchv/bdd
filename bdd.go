136f2a33e2a1c636ca7f78bee627989a7450b3e8
pchchv <ipchchv@gmail.com>
2023-03-14 12:29:25 +0300

    Implement a buffer methods of a format type

--- bdd.go
+++ bdd.go
@@ -1,8 +1,16 @@
 package bdd
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
+)
+
+const (
+	invalidJSON = "null"
+	invalid     = "<nil>"
+	private     = "<private>"
 )
 
 type format struct {
@@ -25,4 +33,69 @@ func (s *format) writeString(str string) {
 
 func (s *format) writeFormat(format string, a ...interface{}) {
 	fmt.Fprintf(s.buf, format, a...)
+}
+
+// nilBuf write buffer with nil
+func (s *format) nilBuf() {
+	switch s.style {
+	case StyleJPrint:
+		s.writeString(invalidJSON)
+	default:
+		s.writeString(invalid)
+	}
+}
+
+// defaultBuf write buffer with default string
+func (s *format) defaultBuf(v reflect.Value) {
+	s.nameBuf(v.Type())
+	switch s.style {
+	case StyleTPrint:
+		s.writeByte('(')
+		s.writeFormat("%#v", v.Interface())
+		s.writeByte(')')
+	case StyleJPrint:
+		d, _ := json.Marshal(v.Interface())
+		s.write(d)
+	default:
+		s.writeFormat("%+v", v.Interface())
+	}
+	return
+}
+
+// nameBuf write buffer with type name
+func (s *format) nameBuf(t reflect.Type) bool {
+	switch s.style {
+	case StyleTPrint:
+		switch t.Kind() {
+		case reflect.Map:
+			s.writeString("map[")
+			s.nameBuf(t.Key())
+			s.writeByte(']')
+			return s.nameBuf(t.Elem())
+		case reflect.Slice:
+			s.writeString("[]")
+			return s.nameBuf(t.Elem())
+		case reflect.Array:
+			s.writeByte('[')
+			s.writeString(strconv.FormatInt(int64(t.Len()), 10))
+			s.writeByte(']')
+			return s.nameBuf(t.Elem())
+		case reflect.Ptr:
+			s.writeByte('*')
+			return s.nameBuf(t.Elem())
+		default:
+			if pkg := t.PkgPath(); pkg != "" {
+				s.writeString(pkg)
+				s.writeByte('.')
+			}
+
+			if t.Name() != "" {
+				s.writeString(t.Name())
+			} else {
+				s.writeString(t.String())
+			}
+			return true
+		}
+	}
+	return false
 }
