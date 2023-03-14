a66cab43da844fa98a67098826ff9883a5eebe04
pchchv <ipchchv@gmail.com>
2023-03-14 13:45:36 +0300

    Implement a strLen and runeWidth funcs

---
+++ misc.go
@@ -0,0 +1,27 @@
+package bdd
+
+import "unicode/utf8"
+
+func runeWidth(r rune) int {
+	switch {
+	case r == utf8.RuneError || r < '\x20':
+		return 0
+	case '\x20' <= r && r < '\u2000':
+		return 1
+	case '\u2000' <= r && r < '\uFF61':
+		return 2
+	case '\uFF61' <= r && r < '\uFFA0':
+		return 1
+	case '\uFFA0' <= r:
+		return 2
+	}
+
+	return 0
+}
+
+func strLen(str string) (i int) {
+	for _, v := range str {
+		i += runeWidth(v)
+	}
+	return
+}
