e63f2d8ffb59cb628352127300b75ac5558b63d4
pchchv <ipchchv@gmail.com>
2023-03-14 12:19:34 +0300

    Implement format type and some of its methods

--- bdd.go
+++ bdd.go
@@ -1,1 +1,28 @@
 package bdd
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type format struct {
+	optional
+	buf    builder
+	filter map[uintptr]bool
+}
+
+func (s *format) write(b []byte) {
+	s.buf.Write(b)
+}
+
+func (s *format) writeByte(b byte) {
+	s.buf.WriteByte(b)
+}
+
+func (s *format) writeString(str string) {
+	s.buf.WriteString(str)
+}
+
+func (s *format) writeFormat(format string, a ...interface{}) {
+	fmt.Fprintf(s.buf, format, a...)
+}
