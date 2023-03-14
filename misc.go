7b691ecf9e5898067b15cac0fbd2e697850d6bd7
pchchv <ipchchv@gmail.com>
2023-03-14 13:58:33 +0300

    implement a spac and spacing funcs

--- misc.go
+++ misc.go
@@ -2,6 +2,8 @@ package bdd
 
 import "unicode/utf8"
 
+var space = map[int]string{}
+
 func runeWidth(r rune) int {
 	switch {
 	case r == utf8.RuneError || r < '\x20':
@@ -24,4 +26,24 @@ func strLen(str string) (i int) {
 		i += runeWidth(v)
 	}
 	return
+}
+
+func spac(depth int) string {
+	b := []byte{}
+	if depth > 0 {
+		for i := 0; i != depth; i++ {
+			b = append(b, Space)
+		}
+	}
+	return string(b)
+}
+
+func spacing(depth int) string {
+	v, ok := space[depth]
+	if ok {
+		return v
+	}
+	v = "\n" + spac(depth-1)
+	space[depth] = v
+	return v
 }
