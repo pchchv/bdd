acd69b1b42e5882cf5b6032aaaaf69b402b6e8ec
pchchv <ipchchv@gmail.com>
2023-03-14 12:10:29 +0300

    Implement a builder type

---
+++ builder.go
@@ -0,0 +1,34 @@
+package bdd
+
+import (
+	"bytes"
+	"sync"
+)
+
+type builder interface {
+	String() string
+	Write([]byte) (int, error)       // io.Writer
+	WriteString(string) (int, error) // io.WriteString
+	WriteByte(byte) error            // io.ByteWriter
+	Reset()
+	Len() int
+}
+
+var poolBuilder = sync.Pool{
+	New: newBuilder,
+}
+
+func newBuilder() interface{} {
+	const malloc = 1024
+	return bytes.NewBuffer(make([]byte, 0, malloc))
+}
+
+func getBuilder() builder {
+	buf := poolBuilder.Get().(builder)
+	return buf
+}
+
+func putBuilder(buf builder) {
+	buf.Reset()
+	poolBuilder.Put(buf)
+}
