7aff5c2ac08cfb54209c814a4356a9d53dbafcc8
pchchv <ipchchv@gmail.com>
2023-03-15 12:48:04 +0300

    Implement a RowPrint test

--- bdd_row_test.go
+++ bdd_row_test.go
@@ -1,11 +1,29 @@
 package bdd
 
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
 func newRowTestData(l int) [][]int {
 	rowtestdata := make([][]int, 0, l)
 	for i := 0; i != l; i++ {
 		rowtestdata = append(rowtestdata, make([]int, i))
 	}
 	return rowtestdata
+}
+
+func TestRowPrint(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	rowtestdata := newRowTestData(100)
+	for _, v := range rowtestdata {
+		Fprint(buf, v)
+	}
+
+	if strings.TrimSpace(buf.String()) != strings.TrimSpace(rowtestdataout) {
+		t.Fail()
+	}
 }
 
 var rowtestdataout = `
